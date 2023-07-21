1c7a01615da3c6f4499f3b857ee333a2c22beaee
Rodrigo Vazquez <rodrigo.e.vazquez@gmail.com>
2023-07-21 01:23:22 -0300

    adaptacion responsive + carrousel + limpieza gral

--- media/java.js
+++ media/java.js
@@ -1,95 +1,50 @@
-var carrousel = {
+/* Carrousel mobile gallery */
 
-    nbSlide : 0,
-    nbCurrent : 1,
-    elemCurrent : null,
-    elem : null,
-    timer : null,
-       
-    init : function(elem){
-        this.nbSlide = elem.find(".slide").length;
-       
-        //creation pagination
-        elem.append('<div class="carrousel-picto"></div>');
-        for(var i=1;i<=this.nbSlide;i++){
-            elem.find(".carrousel-picto").append("<li><span>"+i+"</span></li>");
-        }
-       
-        // au click goto slide correspondant
-        elem.find(".carrousel-picto span").click(function(){
-            carrousel.gotoSlide($(this).text());
-        });
-       
-        //au click prev
-        elem.find("#carrousel-nav .prev").click(function(){
-            carrousel.prev();
-        });
-       
-        //au click next
-        elem.find("#carrousel-nav .next").click(function(){
-            carrousel.next();
-        });
+jQuery(document).ready(function ($) {
 
-        //initialisation du carrousel
-        this.elem = elem;
-        elem.find(".slide").hide();
-        elem.find(".slide:first").show();
-        this.elemCurrent = elem.find(".slide:first");
-        this.elem.find(".carrousel-picto li:first").addClass("active");
-       
-        //créer timer
-        carrousel.play();
+    $('#checkbox').change(function(){
+      setInterval(function () {
+          moveRight();
+      }, 3000);
+    });
+    
+      var slideCount = $('#slider ul li').length;
+      var slideWidth = $('#slider ul li').width();
+      var slideHeight = $('#slider ul li').height();
+      var sliderUlWidth = slideCount * slideWidth;
+      
+      $('#slider').css({ width: slideWidth, height: slideHeight });
+      
+      $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+      
+      $('#slider ul li:last-child').prependTo('#slider ul');
+  
+      function moveLeft() {
+          $('#slider ul').animate({
+              left: + slideWidth
+          }, 200, function () {
+              $('#slider ul li:last-child').prependTo('#slider ul');
+              $('#slider ul').css('left', '');
+          });
+      };
+  
+      function moveRight() {
+          $('#slider ul').animate({
+              left: - slideWidth
+          }, 200, function () {
+              $('#slider ul li:first-child').appendTo('#slider ul');
+              $('#slider ul').css('left', '');
+          });
+      };
+  
+      $('a.control_prev').click(function () {
+          moveLeft();
+      });
+  
+      $('a.control_next').click(function () {
+          moveRight();
+      });
+  
+  });
 
-        //arret au survol du carrousel
-        elem.mouseover(carrousel.stop);
-        elem.mouseout(carrousel.play);
 
-    },
-   
-    gotoSlide : function(num){
-   
-        if(num == this.nbCurrent){return false;}
-
-        /*Animation en slide*/
-        var sens = 1;
-            if(num<this.nbCurrent){sens = -1;}
-        var cssDepart = {"left":sens*this.elem.width()};
-        var cssFin = {"left":sens*-this.elem.width()};
-        this.elem.find("#slide"+num).show().css(cssDepart);
-       
-        this.elem.find("#slide"+num).animate({"top":0,"left":0},500);
-        this.elemCurrent.animate(cssFin,500);
-
-        this.elem.find(".carrousel-picto li").removeClass("active");
-        this.elem.find(".carrousel-picto li:eq("+(num-1)+")").addClass("active");
-        this.nbCurrent = num;
-        this.elemCurrent = this.elem.find("#slide"+num);
-    },
-   
-    next : function(){
-        var num = this.nbCurrent+1;
-        if(num > this.nbSlide){
-            num = 1;
-        }
-        this.gotoSlide(num);
-    },
-    prev : function(){
-        var num = this.nbCurrent-1;
-        if(num <1){
-            num = this.nbSlide;
-        }
-        this.gotoSlide(num);
-    },
-    stop :function(){
-        window.clearInterval(carrousel.timer);
-    },
-    play : function(){
-        window.clearInterval(carrousel.timer);
-        carrousel.timer = window.setInterval("carrousel.next()",5000);
-    }
-   
-}
-
-$(function(){
-    carrousel.init($("#carrousel"));
-});
